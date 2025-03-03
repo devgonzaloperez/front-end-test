105da5e6d28c768c362f278dc485326c1835ef0f
Gonzalo Pérez <gonzalo.perez@amvsoluciones.com>
2025-03-03 12:16:35 +0100

    test: setup testing

---
+++ test/components/Header/Header.test.jsx
@@ -0,0 +1,10 @@
+import {describe, expect, test} from '@jest/globals';
+
+describe("Header", () => {
+
+    test("should render the header", () => {
+        expect(true).toBe(true);
+    });
+
+});
+
